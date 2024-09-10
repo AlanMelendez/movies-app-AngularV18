c882c08e95064845c0f8d8222e201ef6b3baa41b
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2024-09-10 01:15:20 -0600

    add create peliculas-form, this component using create and edit movies

--- src/app/peliculas/peliculas.ts
+++ src/app/peliculas/peliculas.ts
@@ -1,19 +1,19 @@
 // import { actorAutoCompleteDTO } from "../actores/actores";
 
-// export interface PeliculaDTO{
-//     id: number;
-//     titulo: string;
-//     fechaLanzamiento: Date;
-//     trailer: string;
-//     poster?: string;
-// }
+export interface PeliculaDTO{
+    id: number;
+    titulo: string;
+    fechaLanzamiento: Date;
+    trailer: string;
+    poster?: string;
+}
 
-// export interface PeliculaCreacionDTO{
-//     titulo: string;
-//     fechaLanzamiento: Date;
-//     trailer: string;
-//     poster?: File;
-//     generosIds?: number[];
-//     cinesIds?: number[];
-//     actores?: actorAutoCompleteDTO[];
-// }
+export interface PeliculaCreacionDTO{
+    titulo: string;
+    fechaLanzamiento: Date;
+    trailer: string;
+    poster?: File;
+    generosIds?: number[];
+    cinesIds?: number[];
+    // actores?: actorAutoCompleteDTO[];
+}
