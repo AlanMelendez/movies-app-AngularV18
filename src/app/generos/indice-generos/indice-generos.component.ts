0ebf2974c4fcc54e03a855a1fb35dda2b811ab3b
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2024-10-16 08:22:54 -0600

    Add MatTable in IndiceComponent

--- src/app/generos/indice-generos/indice-generos.component.ts
+++ src/app/generos/indice-generos/indice-generos.component.ts
@@ -3,27 +3,34 @@ import { RouterLink } from '@angular/router';
 import { GenerosService } from '../generos.service';
 import { MatButtonModule } from '@angular/material/button';
 import { GeneroDTO } from '../generos';
+import { ListadoGenericoComponent } from '../../compartidos/componentes/listado-generico/listado-generico.component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-indice-generos',
   standalone: true,
-  imports: [RouterLink, MatButtonModule ],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    ListadoGenericoComponent,
+    MatTableModule,
+  ],
   templateUrl: './indice-generos.component.html',
-  styleUrl: './indice-generos.component.css'
+  styleUrl: './indice-generos.component.css',
 })
 export class IndiceGenerosComponent {
-
-  generosService = inject(GenerosService)
+  generosService = inject(GenerosService);
   generos: GeneroDTO[] = [];
+
+  displayedColumns = ['id', 'nombre', 'acciones'];
 
   /**
    *
    */
   constructor() {
-    this.generosService.obtenerTodos().subscribe(generos => {
+    this.generosService.obtenerTodos().subscribe((generos) => {
       this.generos = generos;
       console.log(generos);
     });
   }
-
 }
