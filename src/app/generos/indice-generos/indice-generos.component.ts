ceadb0e759cbd20dcb24123a7c4226fe315aeafa
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2024-10-18 08:35:54 -0600

    Pagination success,created new metods and code refactor for use it pagination

--- src/app/generos/indice-generos/indice-generos.component.ts
+++ src/app/generos/indice-generos/indice-generos.component.ts
@@ -6,6 +6,8 @@ import { GeneroDTO } from '../generos';
 import { ListadoGenericoComponent } from '../../compartidos/componentes/listado-generico/listado-generico.component';
 import { MatTableModule } from '@angular/material/table';
 import { HttpResponse } from '@angular/common/http';
+import { PaginacionDTO } from '../../compartidos/models/PaginacionDTO';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-indice-generos',
@@ -15,6 +17,7 @@ import { HttpResponse } from '@angular/common/http';
     MatButtonModule,
     ListadoGenericoComponent,
     MatTableModule,
+    MatPaginatorModule,
   ],
   templateUrl: './indice-generos.component.html',
   styleUrl: './indice-generos.component.css',
@@ -23,15 +26,31 @@ export class IndiceGenerosComponent {
   generosService = inject(GenerosService);
   generos: GeneroDTO[] = [];
 
+  // Paginacion por defecto
+  paginacion: PaginacionDTO = {pagina: 1,recordsPorPagina: 5};
+  cantidadTotalRegistros: number;
+
   displayedColumns = ['id', 'nombre', 'acciones'];
 
   /**
    *
    */
   constructor() {
-    this.generosService.obtenerPaginacion({pagina:1, recordsPorPagina:5}).subscribe((response: HttpResponse<GeneroDTO[]>) => {
+    this.cargarRegistros(this.paginacion);
+  }
+
+  cargarRegistros(paginacion: PaginacionDTO) {
+    this.generosService.obtenerPaginacion(paginacion).subscribe((response: HttpResponse<GeneroDTO[]>) => {
       this.generos = response.body as GeneroDTO[];
+
+      const header = response.headers.get('cantidad-total-registros') as string;
+      this.cantidadTotalRegistros = parseInt(header);
       console.log(this.generos);
     });
+  }
+
+  actualizarPaginacion(datos: PageEvent) {
+    this.paginacion = {pagina: (datos.pageIndex + 1), recordsPorPagina: datos.pageSize};
+    this.cargarRegistros(this.paginacion);
   }
 }
